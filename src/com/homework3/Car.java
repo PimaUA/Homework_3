c8560bf71d99dedf7c46f2550f58fa3329d23d44
PimaUA <pimaua87@gmail.com>
2022-08-08 19:44:43 +0300

    init commit

---
+++ src/com/homework3/Car.java
@@ -0,0 +1,18 @@
+package com.homework3;
+
+public class Car {
+    public void start() {
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+
+    private void startElectricity() {
+    }
+
+    private void startCommand() {
+    }
+
+    private void startFuelSystem() {
+    }
+}
---
+++ src/com/homework3/Employee.java
@@ -0,0 +1,78 @@
+package com.homework3;
+
+public class Employee {
+    private String name;
+    private String middleName;
+    private String surname;
+    private String rank;
+    private String email;
+    private String phoneNumber;
+    private int age;
+
+    public Employee(String name, String middleName, String surname,
+                    String rank, String email, String phoneNumber, int age) {
+        this.name = name;
+        this.middleName = middleName;
+        this.surname = surname;
+        this.rank = rank;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
